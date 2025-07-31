48f8c95963d8fc742b1ecf2422324ed2da5d7c17
satyam2304 <satyamkumar072005@gmail.com>
2025-07-31 22:05:12 +0530

    added company logo in LatestJobCard

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Badge } from './ui/badge'
 import { useNavigate } from 'react-router-dom'
+import { Button } from './ui/button'
+import { Avatar, AvatarImage } from './ui/avatar'
 
 const LatestJobCards = ({job}) => {
     const navigate = useNavigate();
     return (
         <div onClick={()=> navigate(`/description/${job._id}`)} className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
-            <div>
-                <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
-                <p className='text-sm text-gray-500'>India</p>
+            <div className='flex items-center gap-2 my-2'>
+                <Button className="p-6" variant="outline" size="icon">
+                    <Avatar>
+                        <AvatarImage src={job?.company?.logo} />
+                    </Avatar>
+                </Button>
+                <div>
+                    <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
+                    <p className='text-sm text-gray-500'>India</p>
+                </div>
             </div>
             <div>
                 <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
