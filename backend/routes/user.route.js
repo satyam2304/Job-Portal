f14cad4fe6f83cdcf7958ecb02937ff154bc2c13
satyam2304 <satyamkumar072005@gmail.com>
2025-07-31 20:31:41 +0530

    updated signup

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { multipleUpload } from "../middlewares/multer.js";
+import { multipleUpload , singleUpload} from "../middlewares/multer.js";
  
 const router = express.Router();
 
-router.route("/register").post(multipleUpload, register);
+router.route("/register").post(singleUpload, register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated, multipleUpload, updateProfile);
--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -1,11 +1,14 @@
-import DataUriParser from "datauri/parser.js"
-
+import DataUriParser from "datauri/parser.js";
 import path from "path";
 
 const getDataUri = (file) => {
+    if (!file || !file.originalname || !file.buffer) {
+        return null; // Prevent crashing and return null if input is invalid
+    }
+
     const parser = new DataUriParser();
     const extName = path.extname(file.originalname).toString();
     return parser.format(extName, file.buffer);
-}
+};
 
 export default getDataUri;
